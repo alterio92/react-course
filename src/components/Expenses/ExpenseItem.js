c9c2387b67a5e05a3689578998971e05b61b2018
Agustin Zalazar <agustin.zalazar@mercadolibre.com>
2021-09-20 23:29:00 -0300

    Modify folder structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from '../UI/Card';
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) =>{
+
+ const clickHandler = () => {
+   console.log('Clicked!');
+ }
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 }
 
